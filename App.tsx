0d61d697b82ee386be683dfec78229eaca4043f5
wanxiankai <wanxiankai@126.com>
2022-08-16 23:11:12 +0800

    feat: add state and list

--- App.tsx
+++ App.tsx
@@ -8,20 +8,47 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
+type Task = {
+  name: string;
+  id: number;
+};
 const App = () => {
+  const [currentTask, setCurrentTask] = useState<string>('');
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  const onInput = (itext: string) => {
+    setCurrentTask(itext);
+  };
+
+  const onAddTask = () => {
+    setTaskList(currentTaskList => [
+      ...currentTaskList,
+      {name: currentTask, id: Math.random()},
+    ]);
+    setCurrentTask('');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.taskInput}>
-        <TextInput placeholder="请输入" />
-        <View style={styles.btnGroup}>
-          <Button title="取消" />
-          <Button title="添加" />
-        </View>
+        <TextInput
+          onChangeText={onInput}
+          style={styles.textInput}
+          placeholder="请输入待做事项"
+        />
+        <Button title="添加" onPress={onAddTask} />
       </View>
-      <Text>Hello World</Text>
+      <View style={styles.taskList}>
+        {taskList.map(task => {
+          return (
+            <Text style={styles.taskItem} key={task.id}>
+              {task.name}
+            </Text>
+          );
+        })}
+      </View>
     </View>
   );
 };
@@ -29,19 +56,34 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 50,
+    paddingHorizontal: 16,
   },
   taskInput: {
-    width: '100%',
-    borderColor: 'red',
-    borderWidth: 1,
-  },
-
-  btnGroup: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
+  },
+  taskList: {
+    flex: 5,
+  },
+  taskItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: '#fff',
   },
 });
 
